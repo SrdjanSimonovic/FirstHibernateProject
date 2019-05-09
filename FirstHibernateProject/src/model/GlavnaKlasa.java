59d0f4a32ef259c69dc00cd1174472add8d6f195
oik <oik@LAPTOP-BNEH5OLG>
2019-05-09 19:53:44 +0200

    dodali smo usera

--- FirstHibernateProject/src/model/GlavnaKlasa.java
+++ FirstHibernateProject/src/model/GlavnaKlasa.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.hibernate.Session;
@@ -12,7 +14,7 @@ public class GlavnaKlasa {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+		/*Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Unesite naziv marke.");
 		String nazivMarke = scanner.nextLine();
@@ -29,7 +31,33 @@ public class GlavnaKlasa {
 		scanner.close();
 		
 		CrudMetode metode = new CrudMetode();
-		metode.ubaciMarku(nazivMarke, zemlja, grad, ulica);
+		metode.ubaciMarku(nazivMarke, zemlja, grad, ulica);*/
+		
+		
+		CrudMetode metode = new CrudMetode();
+		
+        metode.ubaciMarku("Moskvic", "Rusija", "Moskva", "KGB");		
+		
+		List<Adresa> listaAdresa = new ArrayList<Adresa>();
+		
+		Adresa adresa1 = new Adresa();
+		adresa1.setZemlja("Srbija");
+		adresa1.setGrad("Beograd");
+		adresa1.setUlica("Knez Mihajlova");
+		
+		listaAdresa.add(adresa1);
+		
+		Adresa adresa2 = new Adresa();
+		adresa2.setZemlja("Srbija");
+		adresa2.setGrad("Nis");
+		adresa2.setUlica("Beogradska");
+		
+		listaAdresa.add(adresa2);
+		metode.ubaciUsera("Pera", "Peric", listaAdresa);
+		
+		
+		
+		
 		
 	}
 
